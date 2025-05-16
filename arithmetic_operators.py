b1764fba80f50ecf716e74897659f108e872671f
enskose <enskose1@gmail.com>
2025-05-16 19:58:11 +0300

    arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,54 @@
+# aritmetik işlemler (+, -, *, /, //, %, **) ile ilgili işlemler yapabiliriz
+
+# toplama işlemi
+toplama1 = 10
+toplama2 = 3
+toplam = toplama1 + toplama2
+print(toplam) # sonuç 13 olacaktır ve burada a ile b değişkeni toplandıktan sonra sonuç c değişkenine atanmaktadır.
+
+# _______________________________________________________________________________________________________________ #
+
+# çıkarma işlemi
+cikarma1 = 10
+cikarma2 = 3
+cikarma = cikarma1 - cikarma2
+print(cikarma) # sonuç 7 olacaktır ve burada a ile b değişkeni çıkarıldıktan sonra sonuç c değişkenine atanmaktadır.
+# _______________________________________________________________________________________________________________ #
+
+# çarpma işlemi
+carpma1 = 10
+carpma2 = 3
+carpma = carpma1 * carpma2
+print(carpma) # sonuç 30 olacaktır ve burada a ile b değişkeni çarpıldıktan sonra sonuç c değişkenine atanmaktadır.
+
+# _______________________________________________________________________________________________________________ #
+
+# bölme işlemi
+bolme1 = 10
+bolme2 = 3
+bolme = bolme1 / bolme2
+print(bolme) # sonuç 3.3333333333333335 olacaktır ve burada a ile b değişkeni bölündükten sonra sonuç c değişkenine atanmaktadır.
+
+# _______________________________________________________________________________________________________________ #
+
+# tam sayı bölme işlemi
+tamBolme1 = 10
+tamBolme2 = 3
+tamBolme = tamBolme1 // tamBolme2
+print(tamBolme) # sonuç 3 olacaktır ve burada a ile b değişkeni tam bölündükten sonra sonuç c değişkenine atanmaktadır.
+
+# _______________________________________________________________________________________________________________ #
+
+# kalan bulma işlemi
+kalanBulma1 = 10
+kalanBulma2 = 3
+kalanBulma = kalanBulma1 % kalanBulma2
+print(kalanBulma) # sonuç 1 olacaktır ve burada a ile b değişkeni bölündükten sonra kalan c değişkenine atanmaktadır.
+
+# _______________________________________________________________________________________________________________ #
+
+# üslü sayılar
+usluSayilar1 = 10
+usluSayilar2 = 3
+usluSayilar = usluSayilar1 ** usluSayilar2
+print(usluSayilar) # sonuç 1000 olacaktır ve burada a ile b değişkeni üslü sayılar olarak hesaplandıktan sonra sonuç c değişkenine atanmaktadır.
