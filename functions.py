ebe424d8c89e261fced40830833beb98f4035049
enskose <enskose1@gmail.com>
2025-05-25 19:21:00 +0300

    functions

--- functions.py
+++ functions.py
@@ -24,6 +24,7 @@ exponentiation_exp_2(5)
 '''
 
 '''
+
 #* Fonksiyonlar, kodu daha düzenli ve tekrar kullanılabilir hale getirir.
 def wage(w_hours):
     return w_hours * 20
@@ -48,12 +49,44 @@ print(result)
 
 '''
 
+#* Fonksiyonlarda if-else yapısı kullanma
+'''
+def add_10(m):
+    if m >= 100:
+        m += 10
+        print("Bonus added! New amount:", m)
+    else:
+        print("Save more money to get a bonus.")
 
-def main():
-    x = int(input("Bir sayı giriniz: "))
-    print("Karesi:", square(x))
-    
-def square(n):
-    return pow(n, 2)
+add_10(50)
 
-main()
+'''
+
+#* Birden fazla parametreye sahip fonksiyonlar
+'''
+def subtract_bc(a, b, c):
+    result = a - b * c
+    print("Paramter b equals:", a)
+    print("Paramter a equals:", b)
+    print("Paramter c equals:", c)
+    print("Result of subtraction:", result)
+    return result
+
+subtract_bc(10, 3, 2)
+'''
+
+#* built-in fonksiyonlar
+
+#* Create a function, called distance_from_zero(), that returns the absolute value of a provided 
+#* single argument and prints a statement "Not possible" if the argument provided is not a number. To solve the task, 
+#* use the type() function in the body of distance_from_zero().
+#* Call the function with the values of -10 and "cat" to verify it works correctly. 
+
+def distance_from_zero(arg):
+    if type(arg) == int:
+        return abs(arg)
+    else:
+        print("Not possible")
+
+print(distance_from_zero(-10))   
+print(distance_from_zero("cat")) 
